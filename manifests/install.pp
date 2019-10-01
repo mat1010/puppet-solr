dda5a4abecfc1d8c878df513eaf45a3fa8fc276b
Frank Wall <fw@moov.de>
2019-10-01 21:30:40 +0200

    run Solr installer with (new) -n parameter

--- manifests/install.pp
+++ manifests/install.pp
@@ -27,17 +27,8 @@ class solr::install {
     $upgrade_flag = ''
   }
 
-  # Prevent SOLR installer from starting the service, because puppet needs
-  # to create/update the configuration files first.
-  $_match_service = 'service "$SOLR_SERVICE" start'
-  augeas { 'remove service start from SOLR installer':
-    lens    => 'simplelines.lns',
-    incl    => "/opt/staging/solr-${solr::version}/bin/install_solr_service.sh",
-    changes => "rm *[.='${_match_service}']",
-    require => Archive["/opt/staging/solr-${solr::version}.tgz"],
-  }
-  ->exec { 'run solr install script':
-    command => "/opt/staging/solr-${solr::version}/bin/install_solr_service.sh /opt/staging/solr-${solr::version}.tgz -i ${solr::extract_dir} -d ${solr::var_dir} -u ${solr::solr_user} -s ${solr::service_name} -p ${solr::solr_port} ${upgrade_flag}",
+  exec { 'run solr install script':
+    command => "/opt/staging/solr-${solr::version}/bin/install_solr_service.sh /opt/staging/solr-${solr::version}.tgz -i ${solr::extract_dir} -d ${solr::var_dir} -u ${solr::solr_user} -s ${solr::service_name} -p ${solr::solr_port} -n ${upgrade_flag}",
     cwd     => "/opt/staging/solr-${solr::version}",
     creates => "${solr::extract_dir}/solr-${solr::version}",
     require => Archive["/opt/staging/solr-${solr::version}.tgz"],
